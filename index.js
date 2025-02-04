fa0222cdb7de8e365fe425439afaf6e63ab71e8c
abrar-431 <abrarh1520444@gmail.com>
2025-02-04 23:50:18 +0600

    project done

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const SSLCommerzPayment = require('sslcommerz-lts')
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { title } = require('process');
+const { default: axios } = require('axios');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -18,6 +20,12 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.urlencoded());
+
+// Payment info
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASS
+const is_live = false
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.salgcrv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -39,6 +47,7 @@ async function run() {
     const jerseyCollection = client.db("jerseyDB").collection("jerseys");
     const cartCollection = client.db('jerseyDB').collection('carts');
     const userCollection = client.db('jerseyDB').collection('users');
+    const orderCollection = client.db('jerseyDB').collection('orders');
 
 
     app.get('/jerseys', async (req, res) => {
@@ -48,9 +57,9 @@ async function run() {
       if (email) {
         query = { providerEmail: email };
       }
-      if(search){
+      if (search) {
         query = {
-          title: {$regex: search, $options: 'i'}
+          title: { $regex: search, $options: 'i' }
         }
       }
       const result = await jerseyCollection.find(query).toArray();
@@ -61,9 +70,9 @@ async function run() {
       let query = {
         category: 'club'
       };
-      if(search){
+      if (search) {
         query = {
-          title: {$regex: search, $options: 'i'},
+          title: { $regex: search, $options: 'i' },
           category: 'club'
         }
       }
@@ -75,9 +84,9 @@ async function run() {
       let query = {
         category: 'local'
       };
-      if(search){
+      if (search) {
         query = {
-          title: {$regex: search, $options: 'i'},
+          title: { $regex: search, $options: 'i' },
           category: 'local'
         }
       }
@@ -85,7 +94,7 @@ async function run() {
       res.send(result);
     })
 
-    
+
     app.get('/jerseys/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -103,16 +112,16 @@ async function run() {
       const jersey = req.body;
       const updateJersey = {
         $set: {
-          title: jersey.title, 
-          category: jersey.category, 
-          image: jersey.image, 
-          price: jersey.price, 
-          description: jersey.description, 
+          title: jersey.title,
+          category: jersey.category,
+          image: jersey.image,
+          price: jersey.price,
+          description: jersey.description,
           fabric_quality: jersey.fabric_quality,
-          available: jersey.available, 
-          version: jersey.version, 
-          sleeve: jersey.sleeve, 
-          design: jersey.design, 
+          available: jersey.available,
+          version: jersey.version,
+          sleeve: jersey.sleeve,
+          design: jersey.design,
           gsm: jersey.gsm
         },
       };
@@ -159,32 +168,32 @@ async function run() {
         next();
       })
     }
-    const verifyAdmin = async(req, res, next)=>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const user = await userCollection.findOne({email: email});
-      const isAdmin = user?.role==='admin';
-      if(!isAdmin){
-        return res.status(403).send({message: 'Unauthorized'})
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'Unauthorized' })
       }
       next();
     }
 
     // user related API
-    app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
-    app.get('/users/admin/:email', verifyToken, async(req, res)=>{
-        const email = req.params.email;
-        if(email !== req.decoded.email){
-          return res.status(403).send({message: 'Forbidden'})
-        }
-        const user = await userCollection.findOne({email: email});
-        let admin = false;
-        if(user?.role === 'admin'){
-          admin = true;
-        }
-        res.send({admin})
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'Forbidden' })
+      }
+      const user = await userCollection.findOne({ email: email });
+      let admin = false;
+      if (user?.role === 'admin') {
+        admin = true;
+      }
+      res.send({ admin })
 
     })
     app.post('/users', async (req, res) => {
@@ -204,7 +213,7 @@ async function run() {
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
-    app.patch('/users/admin/:id',verifyToken, verifyAdmin, async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const updatedUser = {
@@ -223,7 +232,85 @@ async function run() {
       res.send({ token })
     })
 
-    
+    // Payment related API
+    app.post('/create-payment', async (req, res) => {
+      const paymentInfo = req.body;
+      const { name, email, amount, address, city, district, postcode, country, number, productName } = paymentInfo
+      const transaction_id = new ObjectId().toString();
+      const data =
+      {
+        store_id: "jerse67932cc664172",
+        store_passwd: "jerse67932cc664172@ssl",
+        total_amount: amount,
+        currency: "BDT",
+        tran_id: transaction_id,
+        success_url: `https://jersey-geeks-server.vercel.app/success/${transaction_id}`,
+        fail_url: `https://jersey-geeks-server.vercel.app/fail/${transaction_id}`,
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        cus_name: name,
+        cus_email: email,
+        cus_add1: address,
+        cus_city: city,
+        cus_postcode: postcode,
+        cus_country: "Bangladesh",
+        cus_phone: number,
+        shipping_method: "NO",
+        product_name: "Jerseys",
+        product_category: "Jersey",
+        product_profile: "general",
+      }
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+      sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.send({ url: GatewayPageURL })
+        console.log('Redirecting to: ', GatewayPageURL)
+      });
+      const order = {
+        name, email, amount, address, city, district, postcode, country, number, productName, transaction_id, paidStatus: false
+      }
+      const result = await orderCollection.insertOne(order)
+      const deleteCart = await cartCollection.deleteMany({email: email})
+    })
+    app.post('/success/:tran_id', async (req, res) => {
+      const tran_id = req.params.tran_id;
+      console.log(tran_id);
+      const result = await orderCollection.updateOne({ transaction_id: tran_id }, {
+        $set:
+        {
+          paidStatus: true,
+          deliveryStatus: "Pending"
+        }
+      })
+      if (result.modifiedCount > 0) {
+        res.redirect(`https://jersey-geeks.web.app/dashboard/payment/success/${tran_id}`)
+      }
+    })
+    app.post('/fail/:tran_id', async (req, res) => {
+      const tran_id = req.params.tran_id;
+      const result = await orderCollection.deleteOne({ transaction_id: tran_id })
+      if (result.deletedCount > 0) {
+        res.redirect(`https://jersey-geeks.web.app/dashboard/payment/fail/${tran_id}`)
+      }
+    })
+
+    // Order related database
+    app.get('/orders', async (req, res) => {
+      const result = await orderCollection.find().toArray();
+      res.send(result);
+    })
+    app.put('/orders/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const updateOrder = {
+      $set: {
+        deliveryStatus: "Dispatched"
+      }
+    };
+    const result = await orderCollection.updateOne(query, updateOrder);
+    res.send(result)
+    })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
